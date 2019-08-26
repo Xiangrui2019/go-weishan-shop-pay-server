0105f095b235e8b24f166fc7d735c75c846edefd
Go Go Go <xiangrui@aiursoft.com>
2019-08-26 11:21:07 +0800

    add main func

--- services/list_nonpub_order_service.go
+++ services/list_nonpub_order_service.go
@@ -1,5 +1,10 @@
 package services
 
+import "go-weishan-shop-pay-server/serializer"
+
 type ListNonPubOrderService struct {
-	
+}
+
+func (service *ListNonPubOrderService) List() *serializer.Response {
+
 }
