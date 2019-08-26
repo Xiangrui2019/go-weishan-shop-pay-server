c7dd02c91ca1dcbbee0fe25251887eec9c94bc06
Go Go Go <xiangrui@aiursoft.com>
2019-08-26 10:19:20 +0800

    add list order

--- api/order.go
+++ api/order.go
@@ -2,13 +2,22 @@ package api
 
 import (
 	"go-weishan-shop-pay-server/services"
+	"go-weishan-shop-pay-server/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ListOrder(context *gin.Context) {
+	service := services.ListOrderService{}
 
+	if err := context.ShouldBind(&service); err == nil {
+		result := service.List()
+
+		context.JSON(result.Code, result)
+	} else {
+		context.JSON(http.StatusBadRequest, utils.BuildErrorResponse(err))
+	}
 }
 
 func PublishOrder(context *gin.Context) {
