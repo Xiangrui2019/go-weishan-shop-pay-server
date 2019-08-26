6d0312bf0126c3499bb071a889ca64de62d47207
Go Go Go <xiangrui@aiursoft.com>
2019-08-26 11:45:06 +0800

    add nonpub

--- services/list_nonpub_order_service.go
+++ services/list_nonpub_order_service.go
@@ -1,10 +1,45 @@
 package services
 
-import "go-weishan-shop-pay-server/serializer"
+import (
+	"go-weishan-shop-pay-server/models"
+	"go-weishan-shop-pay-server/serializer"
+	"net/http"
+)
 
 type ListNonPubOrderService struct {
+	Limit int `form:"limit"`
+	Start int `form:"start"`
 }
 
 func (service *ListNonPubOrderService) List() *serializer.Response {
+	orders := []models.Order{}
+	total := 0
 
+	if service.Limit == 0 {
+		service.Limit = 6
+	}
+
+	if err := models.DB.Model(models.Order{}).Where("status = ?", true).Count(&total).Error; err != nil {
+		return &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库连接错误",
+			Error:   err.Error(),
+		}
+	}
+
+	if err := models.DB.Where("status = ?", true).Limit(service.Limit).Offset(service.Start).Find(&orders).Error; err != nil {
+		return &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "数据库连接错误",
+			Error:   err.Error(),
+		}
+	}
+
+	return &serializer.Response{
+		Code:    http.StatusOK,
+		Message: "获取订单列表成功.",
+		Data: serializer.BuildDataList(
+			serializer.BuildOrders(orders),
+			uint(total)),
+	}
 }
