85ca52b41bb06e1afca35a70ef30f17b01160b0a
Go Go Go <xiangrui@aiursoft.com>
2019-08-30 15:31:57 +0800

    add bug

--- services/confirm_pay_service.go
+++ services/confirm_pay_service.go
@@ -1,17 +1,14 @@
 package services
 
 import (
+	"encoding/json"
 	"go-weishan-shop-pay-server/cache"
 	"go-weishan-shop-pay-server/global"
-	"go-weishan-shop-pay-server/models"
 	"go-weishan-shop-pay-server/modules"
 	"go-weishan-shop-pay-server/serializer"
+	"go-weishan-shop-pay-server/tasks"
 	"go-weishan-shop-pay-server/utils"
 	"net/http"
-	"os"
-	"strconv"
-
-	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 	"github.com/qingwg/payjs/notify"
@@ -33,72 +30,19 @@ func (service *ConfirmPayService) getOrderCache(msg notify.Message) (*global.Ord
 		return nil, err
 	}
 
+	_, err = cache.CacheClient.Del(msg.Attach).Result()
+	if err != nil {
+		panic(err)
+	}
+
 	return &data, nil
 }
 
-func (service *ConfirmPayService) buildOrder(data *global.OrderCache) *models.Order {
-	return &models.Order{
-		Goodname:    data.Goodname,
-		GoodId:      data.GoodId,
-		Realname:    data.Realname,
-		Address:     data.Address,
-		Phonenumber: data.Phonenumber,
-		ExtInfo:     data.ExtInfo,
-		BuyCount:    data.BuyCount,
-		BuyPrice:    data.BuyPrice,
-		Status:      false,
-		SelfMention: data.SelfMention,
-	}
-}
-
-func (service *ConfirmPayService) buildFee(data *global.OrderCache, to float64, fee float64) *models.Fee {
-	return &models.Fee{
-		TotalValue: data.BuyPrice,
-		ToValue:    to,
-		FeeValue:   fee,
-	}
-}
-
-func (service *ConfirmPayService) createPayRecord(cachedata *global.OrderCache,
-	to float64, fee float64) {
-	tx := models.DB.Begin()
-
-	result := tx.Create(service.buildOrder(cachedata))
-	if result.Error != nil {
-		tx.Rollback()
-		panic(result.Error)
-	}
-
-	err := tx.Create(service.buildFee(cachedata, to, fee)).Error
-	if err != nil {
-		tx.Rollback()
-		panic(err)
-	}
-
-	if err := tx.Commit().Error; err != nil {
-		tx.Rollback()
-		panic(err)
-	}
-}
-
-func (service *ConfirmPayService) messageHandler(context *gin.Context, msg notify.Message) {
+func (service *ConfirmPayService) messageHandler(msg notify.Message) {
 	data, err := service.getOrderCache(msg)
 
-	if err != nil {
-		panic(err)
-	}
+	err = utils.RunAsyncTask(tasks.ConfirmTask, data)
 
-	feerate, err := strconv.ParseFloat(os.Getenv("FEE_RATE"), 64)
-
-	if err != nil {
-		panic(err)
-	}
-
-	to, fee := utils.CalcFee(data.BuyPrice, feerate)
-
-	service.createPayRecord(data, to, fee)
-
-	_, err = cache.CacheClient.Del(msg.Attach).Result()
 	if err != nil {
 		panic(err)
 	}
@@ -108,7 +52,7 @@ func (service *ConfirmPayService) Finish(context *gin.Context) *serializer.Respo
 	payNotify := modules.PayJSModule.GetNotify(context.Request, context.Writer)
 
 	payNotify.SetMessageHandler(func(msg notify.Message) {
-		service.messageHandler(context, msg)
+		service.messageHandler(msg)
 	})
 
 	err := payNotify.Serve()
--- tasks/confirm_task.go
+++ tasks/confirm_task.go
@@ -1,5 +1,72 @@
 package tasks
 
+import (
+	"go-weishan-shop-pay-server/global"
+	"go-weishan-shop-pay-server/models"
+	"go-weishan-shop-pay-server/utils"
+	"os"
+	"strconv"
+)
+
+func buildOrder(data *global.OrderCache) *models.Order {
+	return &models.Order{
+		Goodname:    data.Goodname,
+		GoodId:      data.GoodId,
+		Realname:    data.Realname,
+		Address:     data.Address,
+		Phonenumber: data.Phonenumber,
+		ExtInfo:     data.ExtInfo,
+		BuyCount:    data.BuyCount,
+		BuyPrice:    data.BuyPrice,
+		Status:      false,
+		SelfMention: data.SelfMention,
+	}
+}
+
+func buildFee(data *global.OrderCache, to float64, fee float64) *models.Fee {
+	return &models.Fee{
+		TotalValue: data.BuyPrice,
+		ToValue:    to,
+		FeeValue:   fee,
+	}
+}
+
+func createPayRecord(cachedata *global.OrderCache,
+	to float64, fee float64) error {
+	tx := models.DB.Begin()
+
+	result := tx.Create(buildOrder(cachedata))
+	if result.Error != nil {
+		tx.Rollback()
+		return result.Error
+	}
+
+	err := tx.Create(buildFee(cachedata, to, fee)).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
 func ConfirmTask(data interface{}) error {
+	d := data.(*global.OrderCache)
+
+	feerate, err := strconv.ParseFloat(os.Getenv("FEE_RATE"), 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	to, fee := utils.CalcFee(d.BuyPrice, feerate)
+
+	createPayRecord(d, to, fee)
+
 	return nil
 }
