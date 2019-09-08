94ca755f6b1f477d2838634481b53f3b7f8238db
Go Go Go <xiangrui@aiursoft.com>
2019-09-08 20:59:55 +0800

    add health api to ping

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"go-weishan-shop-pay-server/modules"
 	"go-weishan-shop-pay-server/serializer"
 	"net/http"
 
@@ -8,6 +9,17 @@ import (
 )
 
 func Ping(context *gin.Context) {
+	err := modules.HealthChecksModule.Check()
+
+	if len(err) != 0 {
+		context.JSON(http.StatusInternalServerError, &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "HealthCheck出错",
+			Data:    err,
+		})
+		return
+	}
+
 	context.JSON(http.StatusOK, &serializer.Response{
 		Code:    http.StatusOK,
 		Message: "Pong",
